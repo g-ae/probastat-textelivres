099b5fedd8ef215be753febd2a3b39c3fc88a591
Louis Schanen <louis.schanen@gmail.com>
2025-11-07 09:01:30 +0100

    Count nbr lines deleted and kept

--- etape2_nettoyage.py
+++ etape2_nettoyage.py
@@ -1,8 +1,10 @@
-import re
+import re, os
 
 def nettoyer_part1(filename):    
     with open(filename, encoding='utf-8') as f:
         lines = f.readlines()
+
+        start_lines=len(lines)
 
         # Supprimer tout jusqu'à "Exporté de wikisource"
         array = [l.__contains__('Exporté de Wikisource') for l in lines][::-1]
@@ -40,6 +42,24 @@ def nettoyer_part1(filename):
         # Supprimer toutes les lignes vides
         lines = [l for l in lines if l != "" and l != "\n"]
 
+        end_lines=len(lines)
+
+        deleted_lines = start_lines - end_lines
+        
+
+        # Save to log file
+        log_deleted_lines = -1
+        log_kept_lines = -1
+        genre = filename.split('/')[1]
+        with open(f"book_data/log_{genre}.txt", encoding='utf-8') as l:
+            log_deleted_lines = int(l.readline().split('=')[1])
+            log_kept_lines = int(l.readline().split('=')[1])
+
+        with open(f"book_data/log_{genre}.txt", 'w', encoding='utf-8') as l:
+            l.write(f"deleted_lines={log_deleted_lines+deleted_lines}\n")
+            l.write(f"kept_lines={log_kept_lines+end_lines}")
+            
+
         # Save to file
         # TODO
 
@@ -47,6 +67,14 @@ def nettoyer_part1(filename):
 def nettoyer_part2(filename):
     pass
 
-mouvements = ["lumieres", "naturalisme", "romantisme"]
+if __name__ == "__main__":
+    mouvements = ["naturalisme", "romantisme"]
+    for m in mouvements:
+        onlyfiles = [f for f in os.
+        listdir(f"book_data/{m}") if os.path.isfile(os.path.join(f"book_data/{m}", f))]
+        for text_file in onlyfiles:
+            print(f"book_data/{m}/{text_file}")
+            nettoyer_part1(f"book_data/{m}/{text_file}")
+
 #nettoyer_part1("book_data/romantisme/Atala.txt")
 #nettoyer_part1("book_data/romantisme/Notre-Dame_de_Paris.txt")
